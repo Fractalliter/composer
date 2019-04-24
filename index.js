0cea11f1dc088d8d7d8d584d3e951ec306daa50a
Elias Rahmani <elias.rahmani@chargoon.net>
2019-04-24 19:36:56 +0430

    clean file and remove unnecessery functions

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-var composer = require('./composer');
+var compose = require('./composer');
 
-const add = (...parms) => parms.reduce((a,b) => a+b);
-const mul = (...parms) => parms.reduce((a,b) => a*b); 
-
-composer(mul, add(1,3), 4);
-
-module.exports= {
-    add,
-    mul
-}
+module.exports= compose;
