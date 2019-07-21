e30dacf57b38d30c08e38e27f6f1f438054dabe9
Elias Rahmani <eliasrahmani@outlook.com>
2019-07-21 16:52:16 +0430

    add JSDocs

--- composer.js
+++ composer.js
@@ -1,3 +1,8 @@
+/**
+ * composer is a kind of higher order function for composing two or more functions with each other
+ * @param {Function} f 
+ * @param  {Array<any>} props 
+ */
 const composer = (f, ...props) => f(...props);
 
 module.exports = composer;
